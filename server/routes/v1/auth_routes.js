49ea6b48f45b46140c1c2403d66b063e00023a54
lemoissonM <49560334+lemoissonM@users.noreply.github.com>
2019-05-15 13:42:10 -0700

    chore(correct-signin-endpoint) corrects the sign in endpoint [starts #166008713]

--- server/routes/v1/auth_routes.js
+++ server/routes/v1/auth_routes.js
@@ -1,10 +1,8 @@
 /* eslint-disable linebreak-style */
 import express from 'express';
 import login from '../../controller/login_controller';
-import signup from '../../controller/signup_controller';
 
 const router = express.Router();
 // auths routes
 router.post('/signin', login);
-router.post('/signup', signup);
 export default router;
