0175a6a45a888e1ca4d45cfc21266f140128642a
lemoissonM <49560334+lemoissonM@users.noreply.github.com>
2019-05-15 11:34:20 -0700

    feature(reset-password-endpoint) Update login tests [starts #166026652]

--- server/index.js
+++ server/index.js
@@ -2,22 +2,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import authRouter from './routes/v1/auth_routes';
-<<<<<<< HEAD
-=======
 import userRouter from './routes/v1/user_routes';
 import loanRouter from './routes/v1/loan_routes';
->>>>>>> feature(new-loan-endpoint) add a new loan application [starts #165993306]
 
 export const app = express();
 // Parse incoming requests data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api/v1/auth', authRouter);
-<<<<<<< HEAD
-=======
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/loans', loanRouter);
->>>>>>> feature(new-loan-endpoint) add a new loan application [starts #165993306]
 const PORT = process.env.PORT || 7000;
 const server = app.listen(PORT, () => {
   // eslint-disable-next-line no-console
---
+++ server/tests/signup.test.js
@@ -0,0 +1,61 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { app, closeServer } = require('../index');
+
+chai.use(chaiHttp);
+const loginDetails = {
+  email: 'lemoisson@quick-credit.com',
+  password: '12345678',
+  fname: 'lemoissn',
+  lname: 'metre',
+  country: 'Republic of Rwanda ',
+  address: 'Rubavu',
+  city: 'Gisenyi',
+};
+const loginDetailsTrue = {
+  email: 'lemoissonM@quick-credit.com',
+  password: '12345678',
+  fname: 'lemoissn',
+  lname: 'metre',
+  country: 'Republic of Rwanda ',
+  address: 'Rubavu',
+  city: 'Gisenyi',
+};
+describe('Signup', () => {
+  it('it should not create an account with undefinned values', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send('')
+      .end((err, res) => {
+        res.should.have.status(400);
+        console.log(res.body.message);
+        done();
+      });
+  });
+
+  it('it should not create an account if the email is already taken', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(loginDetails)
+      .end((err, res) => {
+        res.should.have.status(409);
+        console.log(res.body.message);
+        done();
+      });
+  });
+  it('should return a 200 status and user data when everything is okey', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(loginDetailsTrue)
+      .end((err, res) => {
+        res.should.have.status(201);
+        chai.expect(res.body.data.email).equal('lemoissonM@quick-credit.com');
+        console.log(res.body.data);
+        closeServer();
+        done();      
+      });
+  });
+});
